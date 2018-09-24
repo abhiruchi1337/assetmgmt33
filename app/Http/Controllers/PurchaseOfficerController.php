e77eed2a5e0429c8df555014f4f3965d0e3b9831
Abhiruchi <2016.abhiruchi.bhattacharya@ves.ac.in>
2018-09-24 20:02:14 +0530

    po functionality done, entry in requests done, checks availability from store_stock

--- app/Http/Controllers/PurchaseOfficerController.php
+++ app/Http/Controllers/PurchaseOfficerController.php
@@ -4,7 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Indent;
-
+use App\Requests;
+use App\Store_stock;
 class PurchaseOfficerController extends Controller
 {
     public function index(){
@@ -29,7 +30,26 @@ class PurchaseOfficerController extends Controller
         $status=intval($request->input('accept'));
         $original=Indent::where('id',$id)->first();
         $original->status=$status;
+        
+        $request=new Requests();
+        $request->po_id=\Auth::user()->id;
+        $request->item=$original->item;
+        $request->qty=$original->qty;
+        $stock= Store_stock::where('item_name',$original->item)->first();
+        if (isset($stock)){
+            if($stock->qty >= $original->qty){
+                $request->available=1;
+            }
+            else{
+                $request->available=0;
+            }
+        }
+        else{
+            $request->available=0;
+        }
+        $request->save();
         $original->save();
+
         return redirect('/po');
 
     }
